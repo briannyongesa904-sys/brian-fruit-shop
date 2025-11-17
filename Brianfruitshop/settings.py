096668cd48c6176dba9031b7cea2567e9acefd35
Brian Nyongesa <briannyongesa904@gmail.com>
2025-11-17 10:55:21 +0300

    Add static files for deployment

--- Brianfruitshop/settings.py
+++ Brianfruitshop/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(+(tcxvr#1@7o2pvg5k7ros^6eepvl5bn@^b4(oz3y(^bodhoj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['brian-fruit-shop.onrender.com']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
