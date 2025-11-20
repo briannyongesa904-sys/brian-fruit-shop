f10bc2ab170c53159ff9c7c3a5c1a15e7ef803dc
Brian Nyongesa <briannyongesa904@gmail.com>
2025-11-20 13:51:13 +0300

    Add data migration to load products.json

---
+++ products/migrations/0009_load_products.py
@@ -0,0 +1,27 @@
+from django.db import migrations
+import json
+import os
+
+def load_products(apps, schema_editor):
+    Product = apps.get_model('products', 'Product')
+    fixture_path = os.path.join(os.path.dirname(__file__), '../fixtures/products.json')
+    with open(fixture_path, encoding='utf-8') as f:
+        data = json.load(f)
+        for item in data:
+            fields = item['fields']
+            Product.objects.update_or_create(
+                id=item['pk'],
+                defaults=fields
+            )
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('products', '0001_initial'),  # replace 0001_initial with your last migration
+    ]
+
+    operations = [
+        migrations.RunPython(load_products),
+    ]
+
+    ]
